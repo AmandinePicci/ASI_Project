5799619aeea7fdfbe4284c74d68307f97fe9f18a
Amandine <amandine.piccinali@gmail.com>
2022-05-04 17:31:03 +0200

    camarche

---
+++ atelierI/js/creationJSONcard.js
@@ -0,0 +1,28 @@
+ 
+async function postName() {
+
+                                const object = { name: document.querySelector("input[name='name']").value,
+                                    description: document.querySelector("input[name='description']" ).value,
+                                    family: document.querySelector("input[name='family']").value,
+                                    affinity: document.querySelector("input[name='affinity']").value,
+                                    imgUrl: document.querySelector("input[name='imgUrl']").value,
+                                    smallImgUrl: document.querySelector("input[name='smallImgUrl']").value,
+                                    energy: document.querySelector("input[name='energy']").value,
+                                    hp: document.querySelector("input[name='hp']").value,
+                                    defence: document.querySelector("input[name='defence']").value,
+                                    attack: document.querySelector("input[name='attack']").value
+                                }
+                    
+                                ;
+
+                                const response = await fetch('https://asi2-backend-market.herokuapp.com/card', {
+                                                            method: 'POST',     
+                                                            body: JSON.stringify(object),
+                                                            headers: {
+                                                                 'Content-Type': 'application/json'
+                                                                }
+                                                            });
+                                const responseText = await response.text();
+                                //console.log.link ???
+                                console.log(responseText); // logs 'OK'
+}
