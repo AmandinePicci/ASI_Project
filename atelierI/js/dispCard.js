6184b805dfd73e27dbc16a5e3f6fd33c8f294b7a
Cortoz890 <corto.deschamps@club-internet.fr>
2022-05-08 17:18:47 +0200

    affichage de carte après création

--- atelierI/js/dispCard.js
+++ atelierI/js/dispCard.js
@@ -1,14 +1,13 @@
-function loadDoc() {
+window.onload = function() {
     $.ajax({
         type: "Get",
         url: "https://asi2-backend-market.herokuapp.com/cards",
         dataType: 'json',
         success: function(result){
             if(result == []){
-                console.log("Aucune image ne corespond à votre recherche")
+                $("h1:first").text("Aucune carte corespondant n'a été trouvée");
 
             } else{
-
                 var canvas = document.querySelector('.myCanvas');
                 var width = canvas.width = 450;
                 var height = canvas.height = 675;
@@ -23,16 +22,17 @@ function loadDoc() {
 
                 ctx.strokeRect(0, height/10, width, 5*height/10);
                 image.onload = function() {
-                    ctx.drawImage(image, 0, 0, image.width, image.height, 5, height/10, width-10, 5*height/10 - 10);
+                    ctx.drawImage(image, 0, 0, image.width, image.height, 5, height/10, width-6, 5*height/10);
                 }
 
                 ctx.strokeRect(0, 6*height/10, width, 4*height/10);    
-                ctx.fillText(result[result.length - 1].energy, 50, 6*height/10 + 30)
+                ctx.fillText(result[result.length - 1].description, 5, 6*height/10 + 30)
+                ctx.fillText(result[result.length - 1].energy, 30, height - 50)
+                ctx.fillText(result[result.length - 1].hp, 30, height - 20)
+                ctx.fillText(result[result.length - 1].defence, width - 80, height - 50)
+                ctx.fillText(result[result.length - 1].attack, width - 80, height - 20)
                 
             }
-            /*$('#imgPreview').attr('src', result[result.length - 1].imgUrl);
-            console.log(result[result.length - 1].imgUrl);
-            */
        }
        
    })
