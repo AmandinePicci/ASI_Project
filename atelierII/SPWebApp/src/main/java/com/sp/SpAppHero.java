920cee856f32bb231f31040bd0f97f872aaca893
AmandinePicci <72964810+AmandinePicci@users.noreply.github.com>
2022-05-15 22:05:26 +0200

    Add files via upload

---
+++ atelierII/SPWebApp/src/main/java/com/sp/SpAppHero.java
@@ -0,0 +1,26 @@
+package com.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpAppHero {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpAppHero.class,args);
+	}
+}
+
+
+/*package com.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpAppCarte {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpAppCarte.class,args);
+	}
+}*/
---
+++ atelierII/SPWebApp/src/main/java/com/sp/rest/TestRestCrt.java
@@ -0,0 +1,39 @@
+package com.sp.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sp.model.Carte;
+import com.sp.service.CarteService;
+
+
+@RestController
+public class TestRestCrt {
+	
+	@Autowired
+    CarteService cService;
+	
+	
+	
+	//@RequestMapping(method=RequestMethod.POST,value="/addcarte")
+	//public void addHero(@RequestBody Carte carte) {
+		//System.out.println(carte);
+	//}
+	
+	
+	@RequestMapping(method=RequestMethod.GET,value="/msg/id1/id2")
+	public String getMsg(@PathVariable int id1, @PathVariable int id2) {
+		int msg3=id1+id2;
+		return "Msg1:"+"msg2:"+" Somme :"+msg3;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET,value="/parameters")
+	public String getInfoParam(@RequestParam String param1,@RequestParam String param2) {
+		return "Parameters: param1:"+param1+"param2:"+param2;
+	}
+}
