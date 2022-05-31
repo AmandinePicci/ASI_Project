5abc0f82451426951f166d4ad972b4c319247148
AmandinePicci <72964810+AmandinePicci@users.noreply.github.com>
2022-05-31 16:01:51 +0200

    Add files via upload

---
+++ AtelierIII/CarteLib/src/main/java/com/sp/dto/CarteDTOprix.java
@@ -0,0 +1,16 @@
+package com.sp.dto;
+
+public class CarteDTOprix {
+
+	private int id;
+	private int prix;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public int getPrix() {
+		return prix;
+	}
+
+}
---
+++ AtelierIII/CarteService/src/main/java/com/sp/rest/RestCrt.java
@@ -0,0 +1,57 @@
+package com.sp.rest;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sp.dto.CarteDTOprix;
+import com.sp.dto.CarteDto;
+import com.sp.model.Carte;
+import com.sp.service.CarteService;
+
+@RestController
+public class RestCrt {
+	
+	@Autowired
+    CarteService cService;
+    
+    @RequestMapping("/hello")
+    public String sayHello() {
+  	  return "Hello Hero !!!";
+    }
+    
+    @RequestMapping(method=RequestMethod.POST,value="/carte")
+    public void addCarte(@RequestBody CarteDto carteDto) {
+    	Carte carte = new Carte ();
+    	BeanUtils.copyProperties(carteDto, carte);
+        cService.CreationCarte(carte);
+    }
+    
+    @RequestMapping(method=RequestMethod.GET,value="/carte/{id}")
+    public CarteDto getCarte(@PathVariable String id) {
+        Carte c=cService.getCarte(Integer.valueOf(id));
+        CarteDto carteDto = new CarteDto ();
+        BeanUtils.copyProperties(c, carteDto);
+        return carteDto;
+    }
+    
+    @RequestMapping(method=RequestMethod.GET,value="/cartePrix/{id}")
+    public CarteDTOprix getCartePrix(@PathVariable String id) {
+        Carte c=cService.getCarte(Integer.valueOf(id));
+        CarteDTOprix carteDTOprix = new CarteDTOprix ();
+        BeanUtils.copyProperties(c, carteDTOprix);
+        return carteDTOprix;
+    }
+    
+    @RequestMapping(method=RequestMethod.DELETE, value="/delete/{id}")
+    public void deleteCarte(@PathVariable String id) {
+  	  Carte c=cService.getCarte(Integer.valueOf(id));
+  	  cService.SuppressionCarte(c);
+    }
+    
+
+}
