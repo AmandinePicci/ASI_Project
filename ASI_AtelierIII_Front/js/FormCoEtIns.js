e5945febf1d01b256ed6964843a35d2a8ee07394
Amandine <amandine.piccinali@gmail.com>
2022-06-05 21:49:35 +0200

    getMarche

--- ASI_AtelierIII_Front/js/FormCoEtIns.js
+++ ASI_AtelierIII_Front/js/FormCoEtIns.js
@@ -1,9 +1,10 @@
 
 
 async function SubmitFormInscription (){
-    var username = document.getElementById("userName")[0].value;
-    var mdp = document.getElementById("mdp")[0].value;
-    var argent = document.getElementById("argent")[0].value;
+    var username = document.getElementById("userName").value;
+    var mdp = document.getElementById("mdp").value;
+    var argent = document.getElementById("argent").value;
+
 
     var Inscriptionjson = { 
         "userName":username,
@@ -12,6 +13,8 @@ async function SubmitFormInscription (){
         "cardList":[]
     }
 
+    
+    console.log(Inscriptionjson);
 
     const response = await fetch('http://localhost:8081/jeu/addUser', {
                                 method: 'POST',     
@@ -26,6 +29,33 @@ async function SubmitFormInscription (){
 
     
     //window.location.href = "marketing.html";
+
+}
+
+async function SubmitFormConnexion (){
+    var username = document.getElementById("userName").value;
+    var mdp = document.getElementById("mdp").value;
+
+    var lien =  'http://localhost:8081/jeu/getUserByUsername/'+username;
+
+
+    const response = await fetch(lien, {
+                                method: 'GET',    
+                                });
+    const responseText = await response.text();
+    const responseJSON = JSON.parse(responseText);
+
+    var vraimdp = responseJSON["mdp"];
+    if (String(mdp)==String(vraimdp)){
+        
+        window.location.href = "http://127.0.0.1:5500/ASI_AtelierIII_Front/html/marketing.html";
+    }
+    else{
+        var b = document.body;
+        var newTexte = document.createTextNode("Mauvais mot de passe");
+        b.append(newTexte);
+    }
+
 
 }
 
@@ -55,6 +85,9 @@ async function AboutMe(){
     const responseText = await response.text();
 
     console.log(responseText);
-
     
+    var b = document.body;
+    var newTexte = document.createTextNode(responseText);
+    
+    b.append(newTexte);
 }
